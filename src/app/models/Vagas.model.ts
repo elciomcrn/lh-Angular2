41e76ae50c2e788420195696c09407bf99c008cf
elcio.mcrn <elcio.macarini@gmail.com>
2022-03-14 16:23:59 -0300

    Subindo aplicação Angular

---
+++ src/app/models/Vagas.model.ts
@@ -0,0 +1,18 @@
+export class Vagas{
+        id: number = 0;
+        nome: string = "";
+        foto: string = "";
+        descricao: string = "";
+        salario: number = 0;
+
+        construtor(id: number, nome: string, foto: string,
+                descricao: string, salario: number){
+        this.id = id;
+        this.nome = nome;
+        this.foto = foto;
+        this.descricao = descricao;
+        this.salario = salario;
+
+                }
+
+}
